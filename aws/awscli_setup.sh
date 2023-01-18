49e295d65bd4d6ff8fe6bec8c930c4853d8b4ae9
tadokoro <tadokoro@litus.co.jp>
2023-01-18 23:53:41 +0900

    🐛 バックエンドのビルド時のalpineコンテナ内のでaws-cliインストール手順修正

--- aws/awscli_setup.sh
+++ aws/awscli_setup.sh
@@ -1,16 +1,6 @@
 #/bin/sh
 
-# AWS CLI setup
-cd /tmp
-apk --no-cache add binutils curl
-curl -sL https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub -o /etc/apk/keys/sgerrand.rsa.pub
-curl -sLO https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.33-r0/glibc-2.33-r0.apk
-curl -sLO https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.33-r0/glibc-bin-2.33-r0.apk
-apk add --no-cache glibc-2.33-r0.apk glibc-bin-2.33-r0.apk
-curl -sL https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip -o awscliv2.zip
-unzip -q awscliv2.zip
-aws/install
-rm -rf aws*
+apk add aws-cli
 
 mkdir /root/.aws
 cat << 'EOF' > /root/.aws/config
